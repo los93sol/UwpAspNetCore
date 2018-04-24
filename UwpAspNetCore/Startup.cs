4df153efc8d0a23f9c39e70852510e4a38a6f2ca
los93sol <los93sol@gmail.com>
2018-04-24 14:40:54 -0400

    -Bootstrap and shared layout works properly now fixes #2 -Hopefully a less invasive way to light up the wwwroot folder -Workng to get as cloae as possible to the current 2.1 asp.net core web application template

--- UwpAspNetCore/Startup.cs
+++ UwpAspNetCore/Startup.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ApplicationParts;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
-using UwpAspNetLib;
 
 namespace UwpAspNetCore
 {
@@ -33,15 +34,32 @@ namespace UwpAspNetCore
             }
 
             services.AddSingleton(manager);
-            services.AddMvc(options =>
-            {
 
-            });
+            //services.Configure<CookiePolicyOptions>(options =>
+            //{
+            //    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+            //    options.CheckConsentNeeded = context => true;
+            //    options.MinimumSameSitePolicy = SameSiteMode.None;
+            //});
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                //app.UseHsts();
+            }
+
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
+            //app.UseCookiePolicy();
 
             app.UseMvc(routes =>
             {
--- UwpAspNetLib/Controllers/HomeController.cs
+++ UwpAspNetLib/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using UwpAspNetLib.Models;
 
 namespace UwpAspNetLib.Controllers
 {
@@ -7,6 +9,31 @@ namespace UwpAspNetLib.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
